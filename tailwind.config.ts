fb3dc2450160dbbb2b28b342173075c515a5856a
Adrian Bieniek <kontakt@abieniek.dev>
2024-02-12 23:40:13 +0100

    Change primary color and add not-found page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,49 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#067FB3",
-				light: "#FEFEFE",
-				dark: "#141215",
-				error: "#DC2626",
-			},
-			keyframes: {
-				slideDownAndFade: {
-					from: { opacity: "0", transform: "translateY(-2px)" },
-					to: { opacity: "1", transform: "translateY(0)" },
-				},
-				slideLeftAndFade: {
-					from: { opacity: "0", transform: "translateX(2px)" },
-					to: { opacity: "1", transform: "translateX(0)" },
-				},
-				slideUpAndFade: {
-					from: { opacity: "0", transform: "translateY(2px)" },
-					to: { opacity: "1", transform: "translateY(0)" },
-				},
-				slideRightAndFade: {
-					from: { opacity: "0", transform: "translateX(-2px)" },
-					to: { opacity: "1", transform: "translateX(0)" },
-				},
-			},
-			animation: {
-				slideDownAndFade: "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-				slideLeftAndFade: "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-				slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-				slideRightAndFade: "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
-			},
-		},
-	},
-	darkMode: "class",
-	plugins: [],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // primary: "#067FB3",
+        primary: "#458DD1",
+        light: "#FEFEFE",
+        dark: "#141215",
+        error: "#DC2626",
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: "0", transform: "translateY(-2px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        slideLeftAndFade: {
+          from: { opacity: "0", transform: "translateX(2px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        slideUpAndFade: {
+          from: { opacity: "0", transform: "translateY(2px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        slideRightAndFade: {
+          from: { opacity: "0", transform: "translateX(-2px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade:
+          "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade:
+          "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
 };
 export default config;
