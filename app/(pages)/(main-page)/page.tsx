2936910f9ccfbb0a988efe0308fdad90e7d11836
Adrian Bieniek <kontakt@abieniek.dev>
2024-04-25 17:18:12 +0200

    add: header on main page

--- app/(pages)/(main-page)/page.tsx
+++ app/(pages)/(main-page)/page.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import heroImage from "@/public/hero-image.jpg";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello!👋</h1>
-    </div>
+    <header className="mt-10 flex justify-between">
+      <div className="max-w-xl xl:mt-8">
+        <h1 className="mb-4 text-3xl font-bold md:text-4xl lg:text-5xl">
+          Cześć, jestem Adrian!
+        </h1>
+        <p className="mb-4">
+          Na codzień zajmuję się tworzeniem stron i aplikacji internetowych.
+          Studiuję&nbsp;informatykę i rozwijam się jako&nbsp;programista.
+          Chętnie dzielę&nbsp;się wiedzą i&nbsp;doświadczeniem.
+        </p>
+
+        <a
+          href="#kontakt"
+          className="font-semibold text-primary transition-all duration-300 hover:underline"
+        >
+          <i className="ri-mail-send-line mr-2"></i>Skontaktuj się ze mną
+        </a>
+      </div>
+      <Image
+        src={heroImage}
+        alt=""
+        width={350}
+        height={350}
+        className=" hidden rotate-3 rounded-lg xl:block"
+      />
+    </header>
   );
 }
--- app/_hoc/MainLayout.tsx
+++ app/_hoc/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Footer } from "../_components/Footer";
 
 export function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="m-auto flex min-h-screen flex-col justify-between border-x bg-light dark:border-light/10 dark:bg-dark sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl">
+    <div className="m-auto flex min-h-screen flex-col justify-between border-x bg-light text-dark/85 dark:border-light/10 dark:bg-dark dark:text-light/80 sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl">
       <Navbar />
       <main className="px-4 pt-36 sm:px-8 md:px-16">{children}</main>
       <Footer />
