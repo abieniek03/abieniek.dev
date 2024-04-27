2a7f9ec400f8ff7356f1bac651e16f83a26c83b8
Adrian Bieniek <kontakt@abieniek.dev>
2024-04-27 15:16:05 +0200

    add: Vercel Analytics & Cookie Alert

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "remixicon/fonts/remixicon.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { NextThemeProvider } from "./_hoc/NextThemeButton";
 import { MainLayout } from "./_hoc/MainLayout";
+import { CookieAlert } from "./_components/CookieAlert";
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -27,6 +29,8 @@ export default function RootLayout({
         <NextThemeProvider>
           <MainLayout>{children}</MainLayout>
         </NextThemeProvider>
+        <Analytics />
+        <CookieAlert />
       </body>
     </html>
   );
